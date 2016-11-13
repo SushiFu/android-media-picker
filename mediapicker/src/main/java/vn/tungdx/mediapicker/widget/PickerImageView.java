53fd4ebf569114f565265475ee2447c594f8a0bb
Seiji Fouquet <SushiFu@gmx.fr>
2016-11-13 23:45:07 +0100

    Change picker color and width

--- mediapicker/src/main/java/vn/tungdx/mediapicker/widget/PickerImageView.java
+++ mediapicker/src/main/java/vn/tungdx/mediapicker/widget/PickerImageView.java
@@ -3,6 +3,7 @@ package vn.tungdx.mediapicker.widget;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
@@ -33,17 +34,20 @@ public class PickerImageView extends ImageView {
         init();
     }
 
-    private void init() {
-        paintBorder = new Paint();
-        paintBorder.setAntiAlias(true);
-        paintBorder.setColor(getResources().getColor(R.color.picker_color));
-        borderSize = getResources().getDimensionPixelSize(
-                R.dimen.picker_border_size);
-    }
-
     public PickerImageView(Context context) {
         super(context);
         init();
+    }
+
+    private void init() {
+        paintBorder = new Paint();
+        paintBorder.setAntiAlias(true);
+        paintBorder.setColor(ContextCompat.getColor(getContext(), R.color.picker_color));
+        borderSize = getResources().getDimensionPixelSize(R.dimen.picker_border_size);
+    }
+
+    public boolean isSelected() {
+        return isSelected;
     }
 
     public void setSelected(boolean isSelected) {
@@ -51,10 +55,6 @@ public class PickerImageView extends ImageView {
             this.isSelected = isSelected;
             invalidate();
         }
-    }
-
-    public boolean isSelected() {
-        return isSelected;
     }
 
     @Override
@@ -70,10 +70,10 @@ public class PickerImageView extends ImageView {
         if (isSelected) {
             canvas.drawRect(0, 0, borderSize, getHeight(), paintBorder);
             canvas.drawRect(getWidth() - borderSize, 0, getWidth(),
-                    getHeight(), paintBorder);
+                            getHeight(), paintBorder);
             canvas.drawRect(0, 0, getWidth(), borderSize, paintBorder);
             canvas.drawRect(0, getHeight() - borderSize, getWidth(),
-                    getHeight(), paintBorder);
+                            getHeight(), paintBorder);
         }
     }
 }
